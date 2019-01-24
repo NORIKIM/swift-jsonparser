399d97625d438cececd351c529b0642020236b99
김지나 <jinagold.k@gmail.com>
2019-01-24 21:07:58 +0900

    데이터의 개수를 리턴해주던 메소드를 객체로 변경,  객체를 입력 받으면 딕셔너리로 만들어주는 JSONDic객체 생성

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -8,10 +8,49 @@
 
 import Foundation
 
-class JSONData {
+class JSONArr {
     var values: [JsonValue] = []
     
     init(_ values: JsonValue...) {
         self.values = values
     } 
-}	
+}
+
+class JSONDic {
+    var key = [String]()
+    var value = JSONArr().values
+    var data = [String : JsonValue]()
+    
+    init() {
+        for i in 0 ..< min(key.count,value.count) {
+            data[key[i]] = value[i]
+        }
+    }
+
+//    init(key:String, value:JsonValue) {
+//        self.dic = [key : value]
+//    }
+
+}
+
+// 입력된 데이터의 개수를를 딕셔너리에 저장하여 리턴
+class CountData {
+    var data = [String : Int]()
+    init() {
+        self.data = ["문자열" : 0, "숫자" : 0, "부울" : 0]
+    }
+    
+    func countData(in JSON: JSONArr, from splitInput: [String]) -> [String : Int] {
+        for value in JSON.values {
+            if value.isSame(val: String.self) {
+                data["문자열"] = data["문자열"]! + 1
+            } else if value.isSame(val: Int.self) {
+                data["숫자"] = data["숫자"]! + 1
+            } else if value.isSame(val: Bool.self) {
+                data["부울"] = data["부울"]! + 1
+            }
+        }
+        return data
+    }
+    
+}
