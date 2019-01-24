b26f19cc87e0287d8f983b63a97c3478fa086d48
김지나 <jinagold.k@gmail.com>
2019-01-24 21:08:22 +0900

    countData 메소드 삭제

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -12,24 +12,9 @@ struct OutputView {
     private static let totalDataStr = { (splitInput:[String]) -> String in return "총 \(splitInput.count)개의 데이터 중에" }
     private static let included = "가 포함되어 있습니다."
     
-    // 입력된 데이터를 딕셔너리에 저장하여 리턴
-    static func countData(in JSON: JSONData, from splitInput: [String]) -> [String : Int]{
-        var data = ["문자열" : 0, "숫자" : 0, "부울" : 0, ]
-        for value in JSON.values {
-            if value.isSame(val: String.self) {
-                data["문자열"] = data["문자열"]! + 1
-            } else if value.isSame(val: Int.self) {
-                data["숫자"] = data["숫자"]! + 1
-            } else if value.isSame(val: Bool.self) {
-                data["부울"] = data["부울"]! + 1
-            }
-        }
-        return data
-    }
-    
     // 데이터의 개수에 따라 내용 출력
-    static func printData(in JSON: JSONData, from splitInput: [String]) {
-        let dataDic = countData(in: JSON, from: splitInput)
+    static func printData(in JSON: JSONArr, from splitInput: [String]) {
+        let dataDic = CountData().countData(in: JSON, from: splitInput)
         let bool = dataDic["부울"]! > 0 && dataDic["문자열"]! == 0 && dataDic["숫자"]! == 0
         let string = dataDic["부울"]! == 0 && dataDic["문자열"]! > 0 && dataDic["숫자"]! == 0
         let int = dataDic["부울"]! == 0 && dataDic["문자열"]! == 0 && dataDic["숫자"]! > 0
@@ -48,8 +33,8 @@ struct OutputView {
     }
     
     // 모든 데이터의 내용을 출력
-    private static func printDatas(in JSON: JSONData, from splitInput: [String]) {
-        let dataDic = countData(in: JSON, from: splitInput)
+    private static func printDatas(in JSON: JSONArr, from splitInput: [String]) {
+        let dataDic = CountData().countData(in: JSON, from: splitInput)
         var datas = ""
         for (key,value) in dataDic {
             if value != 0 {
@@ -59,3 +44,4 @@ struct OutputView {
         print("\(totalDataStr(splitInput)) \(datas.dropLast(2))\(included)")
     }
 }
+
