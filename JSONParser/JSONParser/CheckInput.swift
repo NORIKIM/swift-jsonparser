1346c7a821d0421bad869f3b1310196f71f6fab1
김지나 <jinagold.k@gmail.com>
2019-01-25 16:55:52 +0900

    공백 확인 메소드 수정(hasWhiteSpace)

--- JSONParser/JSONParser/CheckInput.swift
+++ JSONParser/JSONParser/CheckInput.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 enum ErrorMessage: Error {
+    case whiteSpace
     case reEntered
     var description: String {
         switch(self) {
+        case .whiteSpace: return "문자 사이사이 공백을 넣어주세요!"
         case .reEntered: return """
                                 1. 숫자, 문자, 부울만 가능합니다.
                                 2. 입력값 사이사이 공백을 입력합니다.
@@ -23,18 +25,26 @@ enum ErrorMessage: Error {
 
 struct CheckInput {
     // 사용자의 입력에 공백이 있는지 확인
-    private static func hasWhiteSpace(_ input: String) -> Bool {
-        return input.contains(" ")
+    static func hasWhiteSpace(_ input: String) -> Bool {
+        var comma = 0
+        var whiteSpace = 0
+        
+        for str in input {
+            switch str {
+            case ",":
+                comma += 1
+            case " ":
+                whiteSpace += 1
+            default:
+                break
+            }
+        }
+        return whiteSpace == comma + 2
     }
     
     // 사용자의 입력 양 끝에 [] 확인
-    private static func hasSqaureBracket(_ input: String) -> Bool {
+    static func hasSqaureBracket(_ input: String) -> Bool {
         return input.contains("[") && input.contains("]")
-    }
-
-    // 사용자의 입력이 유효한지 확인
-    static func validInput(userInput: String) -> Bool {
-        return hasWhiteSpace(userInput) && hasSqaureBracket(userInput)
     }
     
     // 사용자의 입력에 { } 확인
@@ -42,4 +52,22 @@ struct CheckInput {
         return input.contains("{") && input.contains("}")
         
     }
+    
+    static func check(_ input: String) {
+        if hasWhiteSpace(input) {
+            if hasCurlyBrace(input) {
+                //makeJsonDic
+                if hasSqaureBracket(input) {
+                    //배열안에 객체 있을 경우 구현
+                }
+            } else {
+                //makeJsonArr
+            }
+        } else {
+            print(ErrorMessage.whiteSpace.description)
+        }
+    }
+    
+    
+    
 }
