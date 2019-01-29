8cb8854eda6db5941cfa966649749c82e1ea9d53
김지나 <jinagold.k@gmail.com>
2019-01-29 17:45:18 +0900

    사용자 입력을 확인하여 조건에 따라 다른 함수 실행하도록 수정

--- JSONParser/JSONParser/main.swift
+++ JSONParser/JSONParser/main.swift
@@ -9,20 +9,26 @@
 import Foundation
 
 func main() {
-    let input = InputView.readInput() 
-
-    guard CheckInput.validInput(userInput: input) else {
-        print(ErrorMessage.reEntered.description)
+    let input = InputView.readInput()
+    let splitInput = InputView.splitInput(input) // ["{ \"name\" : \"KIM JUNG\"", " \"alias\" : \"JK\"", " \"level\" : 5", " \"married\" : true }"]
+    
+    guard CheckInput.hasWhiteSpace(input) else {
+        print(ErrorMessage.whiteSpace.description)
         return
     }
     
-    let splitInput = InputView.splitInput(input)
-    
-    let regex = RegularExpression.makeJsonData(split: splitInput) // [10, "jk", 4, "314", 99, "crong", false]
-    OutputView.printData(in: regex, from: splitInput)
+    if CheckInput.hasCurlyBrace(input) {
+        _ = RegularExpression.makeJsonDic(split: splitInput)
+    } else {
+        _ = RegularExpression.makeJsonArr(split: splitInput)
+    }
+
+    let regex = RegularExpression.makeJsonArr(split: splitInput)
+        
+    OutputView.printData(in: regex, from: splitInput) // 총 4개의 데이터 중에 숫자 1개, 부울 1개, 문자열 2개가 포함되어 있습니다.
 }
 
 while true {
     main()
-}
+} 
 
