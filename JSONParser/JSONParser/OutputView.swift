aee3edcbf5919655c6489424481a344c4c247d00
김지나 <jinagold.k@gmail.com>
2019-02-03 22:05:01 +0900

    사용자 입력에 따라 '객체'출력 여부 결정하는 dataStr메소드의 조건 수정

--- JSONParser/JSONParser/OutputView.swift
+++ JSONParser/JSONParser/OutputView.swift
@@ -9,31 +9,36 @@
 import Foundation
 
 struct OutputView {
-    private static let totalDataStr = { (splitInput:[String]) -> String in return "총 \(splitInput.count)개의 데이터 중에" }
+    private static func dataStr(split: [String], method: ([String]) -> String) -> String {
+        guard split[0].contains("{") else { return method(split) + " 데이터 중에" }
+        return method(split) + " 객체 데이터 중에"
+    }
+    
+    private static let totalDataStr = {(split: [String]) -> String in return "총 \(split.count)개의"}
     private static let included = "가 포함되어 있습니다."
     
     // 데이터의 개수에 따라 내용 출력
-    static func printData(in JSON: JSONArr, from splitInput: [String]) {
-        let dataDic = CountData().countData(in: JSON, from: splitInput)
+    static func printData(in json: [JsonValue], from splitInput: [String]) {
+        let dataDic = CountData().countData(in: json, from: splitInput)
         let bool = dataDic["부울"]! > 0 && dataDic["문자열"]! == 0 && dataDic["숫자"]! == 0
         let string = dataDic["부울"]! == 0 && dataDic["문자열"]! > 0 && dataDic["숫자"]! == 0
         let int = dataDic["부울"]! == 0 && dataDic["문자열"]! == 0 && dataDic["숫자"]! > 0
         
         switch (bool,string,int) {
             case (true, _, _):
-                print("\(totalDataStr(splitInput)) 부울 \(dataDic["부울"]!)개\(included)")
+                print("\(dataStr(split: splitInput, method: totalDataStr)) 부울 \(dataDic["부울"]!)개\(included)")
             case (_, true, _):
-                print("\(totalDataStr(splitInput)) 문자열 \(dataDic["문자열"]!)개\(included)")
+                print("\(dataStr(split: splitInput, method: totalDataStr)) 문자열 \(dataDic["문자열"]!)개\(included)")
             case (_, _, true):
-                print("\(totalDataStr(splitInput)) 숫자 \(dataDic["숫자"]!)개\(included)")
+                print("\(dataStr(split: splitInput, method: totalDataStr)) 숫자 \(dataDic["숫자"]!)개\(included)")
             default:
-                printDatas(in: JSON, from: splitInput)
+                printDatas(in: json, from: splitInput)
             }
             print("")
     }
     
     // 모든 데이터의 내용을 출력
-    private static func printDatas(in JSON: JSONArr, from splitInput: [String]) {
+    private static func printDatas(in JSON: [JsonValue], from splitInput: [String]) {
         let dataDic = CountData().countData(in: JSON, from: splitInput)
         var datas = ""
         for (key,value) in dataDic {
@@ -41,7 +46,7 @@ struct OutputView {
                 datas.append("\(key) \(value)개, ")
             }
         }
-        print("\(totalDataStr(splitInput)) \(datas.dropLast(2))\(included)")
+        print("\(dataStr(split: splitInput, method: totalDataStr)) \(datas.dropLast(2))\(included)")
     }
 }
 
