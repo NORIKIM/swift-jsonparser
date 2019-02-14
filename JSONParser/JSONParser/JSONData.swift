4c59a206876130bfd45e90c69ac82cab7204a575
김지나 <jinagold.k@gmail.com>
2019-02-14 20:05:31 +0900

    JSONData 내 수정사항
    1. JSONDic : key와 value 변수를 추가하고 초기화시킴
    2. CountData: data변수를 private으로 수정하고 함수 내 불필요한 파라미터 삭제

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -17,23 +17,28 @@ class JSONArr {
 }
 
 class JSONDic {
+    var key = [String]()
+    var value = [JsonValue]()
     var data = [String : JsonValue]()
     
     init(key: [String], value: [JsonValue]) {
+        self.key = key
+        self.value = value
         for i in 0 ..< min(key.count, value.count) {
             data[key[i]] = value[i]
         }
     }
 }
 
-// 입력된 데이터의 개수를를 딕셔너리에 저장하여 리턴
+// 입력된 데이터의 개수를 딕셔너리에 저장하여 리턴
 class CountData {
-    var data = [String : Int]()
+    private var data = [String : Int]()
+    
     init() {
         self.data = ["문자열" : 0, "숫자" : 0, "부울" : 0]
     }
     
-    func countData(in JSON: [JsonValue], from splitInput: [String]) -> [String : Int] {
+    func countData(in JSON: [JsonValue]) -> [String : Int] {
         for value in JSON {
             if value.isSame(val: String.self) {
                 data["문자열"] = data["문자열"]! + 1
