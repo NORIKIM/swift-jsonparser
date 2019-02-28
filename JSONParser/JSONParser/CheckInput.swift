edfc5b8975007eae2b2c07c908df881b9be3dd00
김지나 <jinagold.k@gmail.com>
2019-02-28 13:29:32 +0900

    불필요한 hasWhiteSpace메소드 삭제, 입력이 객체를 포함하는 배열인지 확인하는 isObjArr 메소드 추가

--- JSONParser/JSONParser/CheckInput.swift
+++ JSONParser/JSONParser/CheckInput.swift
@@ -22,35 +22,22 @@ enum ErrorMessage: Error {
 }
 
 struct CheckInput {
-    // 사용자의 입력에 공백이 있는지 확인
-    static func hasWhiteSpace(_ input: String) -> Bool {
-        var comma = 0
-        var colon = 0
-        var whiteSpace = 0
-        
-        for index in input {
-            switch index {
-            case ",":
-                comma += 1
-            case ":":
-                colon += 1
-            case " ":
-                whiteSpace += 1
-            default:
-                break
-            }
-        }
-        return whiteSpace == comma + 2 || whiteSpace == (3 * colon) + 2
-    }
-    
     // 사용자의 입력 양 끝에 [] 확인
     static func hasSqaureBracket(_ input: String) -> Bool {
-        return input.contains("[") && input.contains("]")
+        return input.first == "[" && input.last == "]"
     }
     
     // 사용자의 입력에 { } 확인
     static func hasCurlyBrace(_ input: String) -> Bool {
         return input.contains("{") && input.contains("}")
+    }
+    
+    //객체를 포함한 배열인지 확인
+    static func isObjArr(_ input: String) -> Bool {
+        if hasCurlyBrace(input) {
+            return hasSqaureBracket(input)
+        }
+        return false
     }
 
 }
