ea6f98f2c77212c4609412a2ea66af012ebd50db
김지나 <jinagold.k@gmail.com>
2019-03-01 16:13:58 +0900

    변경 사항
    1. JSONArr : JsonCollection 채택
    2. JSONDic : JsonCollection 채택,
                          key/value 변수와 초기화 부분 삭제
                          딕셔너리 value만 가지는 json배열 변수 생성
    3. CountData : 객체와 배열 개수 가지도록 수정

--- JSONParser/JSONParser/JSONData.swift
+++ JSONParser/JSONParser/JSONData.swift
@@ -8,47 +8,56 @@
 
 import Foundation
 
-class JSONArr {
+class JSONArr: JsonCollection {
     var values: [JsonValue] = []
     
     init(_ values: JsonValue...) {
         self.values = values
     }
+    
+    func countData() -> [String : Int] {
+        return CoundData.calculate(values)
+    }
 }
 
-class JSONDic {
-    var key = [String]()
-    var value = [JsonValue]()
+class JSONDic: JsonCollection {
     var data = [String : JsonValue]()
     
-    init(key: [String], value: [JsonValue]) {
-        self.key = key
-        self.value = value
-        for i in 0 ..< min(key.count, value.count) {
-            data[key[i]] = value[i]
+    init(_ data: [String: JsonValue]) {
+        self.data = data
+    }
+    
+    var json: [JsonValue] {
+        var value = [JsonValue]()
+        for val in data.values {
+            value.append(val)
         }
+        return value
+    }
+    
+    func countData() -> [String : Int] {
+        return CoundData.calculate(json)
     }
 }
 
-// 입력된 데이터의 개수를 딕셔너리에 저장하여 리턴
-class CountData {
-    private var data = [String : Int]()
-    
-    init() {
-        self.data = ["문자열" : 0, "숫자" : 0, "부울" : 0]
-    }
-    
-    func countData(in JSON: [JsonValue]) -> [String : Int] {
-        for value in JSON {
-            if value.isSame(val: String.self) {
-                data["문자열"] = data["문자열"]! + 1
+class CoundData {
+    static func calculate(_ json: [JsonValue]) -> [String: Int] {
+        var data = ["문자열" : 0, "숫자" : 0, "부울" : 0, "객체" : 0, "배열": 0]
+        
+        for value in json {
+            if value.isSame(val: Dictionary<String,JsonValue>.self){
+                data["배열"] = data["배열"]! + json.count / 2
             } else if value.isSame(val: Int.self) {
                 data["숫자"] = data["숫자"]! + 1
             } else if value.isSame(val: Bool.self) {
                 data["부울"] = data["부울"]! + 1
+            } else if value.isSame(val: String.self) {
+                data["문자열"] = data["문자열"]! + 1
+            }  else { // 객체로 인식하도록 처리를 어떻게 해야 할지 모르겠습니다.
+                data["객체"] = data["객체"]! + 1
             }
         }
+        print(data)
         return data
     }
-    
 }
